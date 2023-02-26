98c3d02f7e6c2102733aa81edd12659b9f51c7a9
BayC <rachada_32@hotmail.com>
2023-02-25 22:34:05 -0500

    Add a tile layer

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // initialize a map and set its view to geographical coordinates and a zoom level:
 const map = L.map('map').setView([51.505, -0.09], 13);
+
+// add a tile layer with the maximum zoom level and the attribution text
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
